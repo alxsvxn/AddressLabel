dd4de9454f231a6b280d133beaba3fe4a84c1aea
Alexis Villagran <alexisvillagran18@hotmail.com>
2025-03-23 21:52:48 -0600

    Added Event Handlers

--- Address Label/AddressLabel.vb
+++ Address Label/AddressLabel.vb
@@ -1,4 +1,7 @@
 ﻿'Alexis Villagran
+'RCET 2265
+'Spring 2025
+'Address Label
 
 Option Strict On
 Option Explicit On
@@ -12,6 +15,36 @@ Public Class AddressLabel
         ZipCodeTextBox.Text = ""
         AddressLabelGroupbox.Text = "Enter your details and click Display Label."
         FirstNameTextBox.Focus()
+    End Sub
+
+    Sub SetFormattedAddress()
+
+        Dim firstName As String = FirstNameTextBox.Text.Trim()
+        Dim lastName As String = LastNameTextbox.Text.Trim()
+        Dim street As String = StreetAddressTextBox.Text.Trim()
+        Dim city As String = CityTextBox.Text.Trim()
+        Dim state As String = StateTextBox.Text.Trim()
+        Dim zip As String = ZipCodeTextBox.Text.Trim()
+
+        Dim fullName As String = $"{firstName} {lastName}"
+        Dim cityStateZip As String = $"{city}, {state} {zip}"
+        AddressLabelGroupBox.Text = $"{fullName}{vbCrLf}{street}{vbCrLf}{cityStateZip}"
+
+    End Sub
+
+    ' -----------------Event Handlerss-----------------
+    Private Sub DisplayButton_Click(sender As Object, e As EventArgs) Handles DisplayButton.Click
+        SetFormattedAddress()
+    End Sub
+    Private Sub AddressLabelForm_Load(sender As Object, e As EventArgs) Handles Me.Load
+        SetDefaults()
+    End Sub
+
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
+    Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
+        SetDefaults() ' Reset all fields
     End Sub
 
 End Class
--- Address Label/My Project/Application.Designer.vb
+++ Address Label/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Address_Label.Form1
+            Me.MainForm = Global.Address_Label.AddressLabel
         End Sub
     End Class
 End Namespace
